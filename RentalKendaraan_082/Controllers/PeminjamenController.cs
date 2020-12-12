3537e449e74d3b1113ec0087a3d6b439b3485de5
UB8082 <lucky.ahmad.ft18@mail.umy.ac.id>
2020-12-12 12:20:00 +0700

    Add Role

--- RentalKendaraan_082/Controllers/PeminjamenController.cs
+++ RentalKendaraan_082/Controllers/PeminjamenController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RentalKendaraan_082.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RentalKendaraan_082.Controllers
 {
@@ -18,6 +19,7 @@ namespace RentalKendaraan_082.Controllers
             _context = context;
         }
 
+        [Authorize(Policy = "readonlypolicy")]
         // GET: Peminjamen
         public async Task<IActionResult> Index(string ktsd, string searchString, string sortOrder, string currentFilter, int? pageNumber)
         {
@@ -108,6 +110,7 @@ namespace RentalKendaraan_082.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy = "writepolicy")]
         // GET: Peminjamen/Create
         public IActionResult Create()
         {
@@ -136,6 +139,7 @@ namespace RentalKendaraan_082.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy = "editpolicy")]
         // GET: Peminjamen/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
--- RentalKendaraan_082/Startup.cs
+++ RentalKendaraan_082/Startup.cs
@@ -13,7 +13,7 @@ using RentalKendaraan_082.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RentalKendaraan_082.Models;
-
+using Microsoft.AspNetCore.Identity.UI.Services;
 namespace RentalKendaraan_082
 {
     public class Startup
@@ -43,6 +43,21 @@ namespace RentalKendaraan_082
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultUI().AddEntityFrameworkStores<rental_kendaraanContext>().AddDefaultTokenProviders();
+
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("readonlypolicy",
+                    builder => builder.RequireRole("Admin", "Manager", "Kasir"));
+                options.AddPolicy("writepolicy",
+                    builder => builder.RequireRole("Admin","Kasir"));
+                options.AddPolicy("editpolicy",
+                    builder => builder.RequireRole("Admin", "Kasir"));
+                options.AddPolicy("deletepolicy",
+                    builder => builder.RequireRole("Admin", "Kasir"));
+            });
+            services.AddScoped<Peminjaman>();
+            services.AddScoped<Pengembalian>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
